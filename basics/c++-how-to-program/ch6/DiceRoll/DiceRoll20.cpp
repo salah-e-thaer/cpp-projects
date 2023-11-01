f7f4193bff26df9e8883ebff610c8c8e9b45727f
slah e thaer <salah.e.thaer@gmail.com>
2023-11-01 13:54:07 +0200

    add 'DiceRoll' folder

---
+++ basics/c++-how-to-program/ch6/DiceRoll/DiceRoll20.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    for (unsigned int counter{1}; counter <= 20; ++counter) {
+        cout << setw(10) << (1 + rand() % 6);
+
+        if (counter % 5 == 0) {
+            cout << endl;
+        }
+    }
+}
