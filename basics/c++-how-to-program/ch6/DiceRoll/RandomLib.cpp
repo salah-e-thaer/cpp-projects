a932d2d7183077c70b3cc6927e89c9186c434509
slah e thaer <salah.e.thaer@gmail.com>
2023-11-04 20:37:45 +0200

    adding 'RandomLib.cpp' to 'DiceRoll' folder

---
+++ basics/c++-how-to-program/ch6/DiceRoll/RandomLib.cpp
@@ -0,0 +1,20 @@
+// Using a random-number generation engine and distribution
+// to roll  a six-sided die.
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <ctime>
+using namespace std;
+
+int main() {
+    default_random_engine engine{static_cast<unsigned int>(time(0))};
+    uniform_int_distribution<unsigned int> randomInt{1, 6};
+
+    for (unsigned int counter{1}; counter <= 10; ++counter) {
+        cout << setw(10) << randomInt(engine);
+
+        if (counter % 5 == 0) {
+            cout << endl;
+        }
+    }
+}
