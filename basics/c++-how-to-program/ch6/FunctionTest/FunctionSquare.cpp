6d87cfec1b401367d75f99d62df10ed44b8fe28e
slah e thaer <salah.e.thaer@gmail.com>
2023-11-06 20:14:42 +0200

    adding 'FunctionSqure.cpp' to 'FunctionTest' folder

---
+++ basics/c++-how-to-program/ch6/FunctionTest/FunctionSquare.cpp
@@ -0,0 +1,15 @@
+// square function.
+#include <iostream>
+using namespace std;
+
+int square(int);
+
+int main() {
+    int a{10};
+
+    cout << a << " squared: " << square(a) << endl;
+}
+
+int square(int x) {
+    return x * x;
+}
