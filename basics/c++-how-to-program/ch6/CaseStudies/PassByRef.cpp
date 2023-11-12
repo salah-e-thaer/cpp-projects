e2bbafabe8b5f6c79a055630c5fd82b314d1b745
slah e thaer <salah.e.thaer@gmail.com>
2023-11-12 13:02:25 +0200

    adding 'PassByRef.cpp' to 'CaseStudies' folder

---
+++ basics/c++-how-to-program/ch6/CaseStudies/PassByRef.cpp
@@ -0,0 +1,27 @@
+// Passing arguments by value and by reference.
+#include <iostream>
+using namespace std;
+
+int squareByValue(int);
+void squareByReference(int&);
+
+int main() {
+    int x{2};
+    int z{4};
+
+    cout << "x= " << x << " before squareByValue\n";
+    cout << "Value returned by squareByValue: " << squareByValue(x) << endl;
+    cout << "x= " << x << " after squareByValue\n" << endl;
+
+    cout << "z = " << z << " before squareByReference" << endl;
+    squareByReference(z);
+    cout << "z = " << z << " after squareByReference" << endl;
+}
+
+int squareByValue(int number) {
+    return number *= number;
+}
+
+void squareByReference(int& numberRef) {
+    numberRef *= numberRef;
+}
