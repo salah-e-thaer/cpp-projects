b63ef04f8408e265f01fdf0f330c5908fbed4ca1
slah e thaer <salah.e.thaer@gmail.com>
2023-11-13 14:15:40 +0200

    doing fibonacci series with recursion in 'Recursion' folder

---
+++ basics/c++-how-to-program/ch6/Recursion/Fibonacci.cpp
@@ -0,0 +1,25 @@
+// Recursive function fibonacci.
+#include <iostream>
+using namespace std;
+
+unsigned long fibonacci(unsigned long);
+
+int main() {
+    for (unsigned int counter{0}; counter <= 10; ++counter) {
+        cout << "fibonacci(" << counter << ")" << fibonacci(counter) << endl;
+    }
+
+    // display higher fibonacci values.
+    cout << "\nfibonacci(20) = " << fibonacci(20) << endl;
+    cout << "fibonacci(30) = " << fibonacci(30) << endl;
+    cout << "fibonacci(35) = " << fibonacci(35) << endl;
+}
+
+unsigned long fibonacci(unsigned long number) {
+    if ((0 == number) || (1 == number)) {
+        return number;
+    }
+    else {
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
+}
