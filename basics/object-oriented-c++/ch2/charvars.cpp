48c159c010b9f1c0c374a7fa8a0f856630b02481
slah e thaer <salah.e.thaer@gmail.com>
2023-11-14 21:48:39 +0200

    starting in the second book of c++ basics

---
+++ basics/object-oriented-c++/ch2/charvars.cpp
@@ -0,0 +1,17 @@
+// demonstrates character variables.
+#include <iostream>
+using namespace std;
+
+int main() {
+    char charVar1 = 'A';
+    char charVar2 = '\t';
+
+    cout << charVar1;
+    cout << charVar2;
+
+    charVar1 = 'B';
+    cout << charVar1;
+    cout << '\n';
+
+    return 0;
+}
---
+++ basics/object-oriented-c++/ch2/intvars.cpp
@@ -0,0 +1,16 @@
+// demonstrates integer variables.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int var1;
+    int var2;
+
+    var1 = 20;
+    var2 = var1 + 10;
+
+    cout << "var1+10 is ";
+    cout << var2 << endl;
+
+    return 0;
+}
