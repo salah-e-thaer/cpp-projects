560be578f44094f5452a4e546d331bb5c1dc3958
slah e thaer <salah.e.thaer@gmail.com>
2023-11-15 09:20:32 +0200

    finishing chapter2

---
+++ basics/object-oriented-c++/ch2/libFunctions/sqrt.cpp
@@ -0,0 +1,16 @@
+// demonstrates sqrt() library function.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double number, answer;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    answer = sqrt(number);
+    cout << "Square root is " << answer << endl;
+
+    return 0;
+}
